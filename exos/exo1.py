0e232b8ba784f6355f2bbfad4ccbc97db9cc94e5
Auriane <auriane.b8@gmail.com>
2026-03-05 19:30:00 +0200

    Add python exercise 1

---
+++ exos/exo1.py
@@ -0,0 +1,51 @@
+first = "Hello world"
+
+#this is a commmment
+
+print("I AM A COMPUTER!")
+
+if 1 < 2 and 4 > 2:
+   print("Math is fun")
+   
+nope = ""
+
+True and False
+
+print(len("What's my length?"))
+
+print("i am shouting".upper())
+
+int("1000")
+
+print(f"{4}real")
+
+print(3*"cool")
+
+#print(1/0)
+#ZeroDivisionError: division by zero
+
+
+print(type([]))
+
+name = input("What is your name?: ")
+
+number = int(input("Pick a number: "))
+if number<0 :
+    print("This number is less than 0!")
+elif number>0:
+    print("This number is more than 0!")
+else : 
+    print("You picked 0")
+   
+   
+string = "apple"  
+print(string[-2])
+
+print("y" in "xylophone")
+
+my_string = "my string"
+my_string.islower()
+
+   
+   
+   
